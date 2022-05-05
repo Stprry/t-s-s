ff6948f443b45bd09a156bf0c71f3c703adee870
Sperry.dev <samperry.dev@gmail.com>
2022-05-05 22:58:13 +0100

    Margin bottom image

--- components/grid-item.tsx
+++ components/grid-item.tsx
@@ -5,7 +5,7 @@ import NextLink from 'next/link'
 export const GridItem = ({ thumbnail, href, title, textBody, btnName, id }) => (
   <div className="card  rounded-md bg-black shadow-xl lg:card-side">
     <figure className="rounded-md border-b-4 border-solid border-white  lg:border-b-0 lg:border-r-4">
-      <span className="m-0 mb-2 border-4 border-solid border-gray-300  lg:border-0 ">
+      <span className="mb-2 border-4 border-solid border-gray-300 lg:m-0  lg:border-0 ">
         <div style={{ width: '200px', height: '200px', position: 'relative' }}>
           <Image src={thumbnail} alt={title} layout="fill" />
         </div>
